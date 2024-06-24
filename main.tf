385f59beb4de270f80df9c1aa6e5fcf2f3dfbef5
logan222897 <67660388+logan222897@users.noreply.github.com>
2024-06-25 00:03:51 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -8,18 +8,17 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_postgresql_flexible_server" "example" {
-  name                = "vt5
+  name                = "example-flexible-server"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
   administrator_login          = "devops_dba"
-  administrator_password       = "Larak2013"  // Hardcoded password
+  administrator_password       = "Larak2013"
   version                      = "12"
-  sku_name                     = "Standard_E32s_v3"
+  sku_name                     = "Standard_D2s_v3"
   storage_mb                   = 32768
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
+
+  // Other configurations as needed
 }
-
-
-
