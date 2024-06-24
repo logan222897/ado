1cc1079e7c9d16983f1df6eadb2dfd2ee3fc3163
logan222897 <67660388+logan222897@users.noreply.github.com>
2024-06-24 23:38:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_postgresql_flexible_server" "example" {
-  name                = "example-flexible-server"
+  name                = "vt5
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
