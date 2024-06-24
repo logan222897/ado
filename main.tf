4856725c14372a32211b1790193ebcaa66e1d1ea
logan222897 <67660388+logan222897@users.noreply.github.com>
2024-06-24 22:33:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,11 @@ resource "azurerm_postgresql_flexible_server" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
-  administrator_login          = "devops_dba"
-  administrator_password       = "Larak2013"
-  version                      = "12"
-  sku_name                     = "Standard_D2s_v3"
+  administrator_login    = "devops_dba"
+  administrator_password = "Larak2013"
+  version                = "12"
+  sku_name               = "GP_Gen5_2"  # Example of a valid SKU name
+
   storage_mb                   = 32768
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
