114859f18429d2ae87ed506f4444a7e99de01057
logan222897 <67660388+logan222897@users.noreply.github.com>
2024-06-24 23:11:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,19 @@ resource "azurerm_postgresql_flexible_server" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
-  administrator_login    = "devops_dba"
-  administrator_password = "Larak2013"
-  version                = "12"
-  sku_name               = "GP_Gen5_2"  # Example of a valid SKU name
-
+  administrator_login          = "devops_dba"
+  administrator_password       = var.db_admin_password  // Use a Terraform variable to set this securely
+  version                      = "12"
+  sku_name                     = "Standard_E32s_v3"  // Updated SKU
   storage_mb                   = 32768
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
 
   // Other configurations as needed
+}
+
+variable "db_admin_password" {
+  description = "The administrator password for PostgreSQL Flexible Server"
+  type        = string
+  sensitive   = true
 }
